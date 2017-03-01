4e98bde0e6c6b740f334496c9a7702bc6c1893a0
Stephan Scholz <stephan.scholz@gmx.at>
2017-03-01 15:49:21 +0100

    Basic WebRequest, getting a user_token.

--- Brickventory/BrickventoryApp/LoginActivity.cs
+++ Brickventory/BrickventoryApp/LoginActivity.cs
@@ -9,6 +9,10 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using System.Net;
+using System.IO;
+using UserData;
+using Newtonsoft.Json;
 
 namespace BrickventoryApp
 {
@@ -25,7 +29,37 @@ namespace BrickventoryApp
 
         private void Login_Click(object sender, System.EventArgs e)
         {
-            Toast.MakeText(this, "Login", ToastLength.Long).Show();
+            Create_UserToken();
+        }
+
+        private void Create_UserToken()
+        {
+            string username, password;
+            username = FindViewById<EditText>(Resource.Id.tf_username).Text;
+            password = FindViewById<EditText>(Resource.Id.tf_password).Text;
+            Toast.MakeText(this, "user: " + username + ", pw: " + password, ToastLength.Long).Show();
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://rebrickable.com/api/v3/users/_token/?key=faaf91147e37117cfe0d94519f8fde41");
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Accept = "application/json";
+            string parameters = "username=" + username + "&password=" + password;
+
+            using (StreamWriter streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+
+                streamWriter.Write(parameters);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            HttpWebResponse httpResponse = (HttpWebResponse)request.GetResponse();
+            using (StreamReader streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                User user = JsonConvert.DeserializeObject<User>(result);
+                Toast.MakeText(this, "usertoken: " + user.user_token, ToastLength.Long);
+            }
         }
     }
 
