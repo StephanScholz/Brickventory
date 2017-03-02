4efa0b4d47a1204fb01ff420394e994df7a77239
Stephan Scholz <stephan.scholz@gmx.at>
2017-03-02 11:45:47 +0100

    Created a Class for easier Access to WebRequests

--- Brickventory/BrickventoryApp/LoginActivity.cs
+++ Brickventory/BrickventoryApp/LoginActivity.cs
@@ -14,6 +14,8 @@ using System.IO;
 using UserData;
 using Newtonsoft.Json;
 
+using BrickventoryApp.Resources.classes.Utility;
+
 namespace BrickventoryApp
 {
     [Activity(Label = "Login")]
@@ -29,39 +31,9 @@ namespace BrickventoryApp
 
         private void Login_Click(object sender, System.EventArgs e)
         {
-            Create_UserToken();
-        }
-
-        private void Create_UserToken()
-        {
-            string username, password;
-            username = FindViewById<EditText>(Resource.Id.tf_username).Text;
-            password = FindViewById<EditText>(Resource.Id.tf_password).Text;
-            //Toast.MakeText(this, "user: " + username + ", pw: " + password, ToastLength.Long).Show();
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://rebrickable.com/api/v3/users/_token/?key=faaf91147e37117cfe0d94519f8fde41");
-            request.Method = "POST";
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.Accept = "application/json";
-            request.Credentials = CredentialCache.DefaultNetworkCredentials;
-            string parameters = "username=" + username + "&password=" + password;
-
-            using (StreamWriter streamWriter = new StreamWriter(request.GetRequestStream()))
-            {
-
-                streamWriter.Write(parameters);
-                streamWriter.Flush();
-                streamWriter.Close();
-            }
-
-            HttpWebResponse httpResponse = (HttpWebResponse)request.GetResponse();
-            using (StreamReader streamReader = new StreamReader(httpResponse.GetResponseStream()))
-            {
-                string result = streamReader.ReadToEnd();
-                User user = JsonConvert.DeserializeObject<User>(result);
-                Toast.MakeText(this, "Connected!!!", ToastLength.Long).Show();
-                Toast.MakeText(this, "usertoken: " + user.user_token, ToastLength.Long).Show();
-            }
+            string username = FindViewById<EditText>(Resource.Id.tf_username).Text;
+            string password = FindViewById<EditText>(Resource.Id.tf_password).Text;
+            RequestUtility.Create_UserToken(username, password);
         }
     }
 
---
+++ Brickventory/BrickventoryApp/Resources/Classes/Utility/RequestUtility.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Net;
+using System.IO;
+using UserData;
+using Newtonsoft.Json;
+
+// A Utility-Class, that makes Access and Usage of WebRequests easier
+namespace BrickventoryApp.Resources.classes.Utility
+{
+    public static class RequestUtility
+    {
+
+        private const string API_KEY = "faaf91147e37117cfe0d94519f8fde41";
+
+        public static string Create_UserToken(string username, string password)
+        {
+            string parameters = "username=" + username + "&password=" + password;
+
+            return POST_Request("http://rebrickable.com/api/v3/users/_token/" + API_KEY, parameters);
+        }
+
+        private static string POST_Request(string url, string parameters)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Accept = "application/json";
+            request.Credentials = CredentialCache.DefaultNetworkCredentials;
+
+            using (StreamWriter streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+
+                streamWriter.Write(parameters);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            HttpWebResponse httpResponse = (HttpWebResponse)request.GetResponse();
+            using (StreamReader streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                return streamReader.ReadToEnd();
+            }
+        }
+
+    }
+}
